b04004a21bb8f397776ff34f5eafe413fd6da153
lyelc <lyeclyde@gmail.com>
2022-08-20 15:36:24 +0545

    modified helper

--- components/Table.js
+++ components/Table.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {FiEdit, FiTrash2} from 'react-icons/fi'
-import data from '../db/data.json';
-import { getUser } from '../lib/helper';
+import { getUsers } from '../lib/helper';
+import {useQuery} from 'react-query'; 
 
 export default function Table() {
-    getUser().then(res=>console.log(res));
+    const {isLoading, isError, data, error} = useQuery("users", getUsers)
+    if(isLoading) return <div>Employee is loading</div>
+    if(isError) return <div>Got Error {error}</div>
   return (
     <table className='min-w-full table-auto'>
         <thead>
@@ -43,19 +45,19 @@ function Tr({id, name, avatar, email, salary, date, status }) {
         <tr className='bg-gray-50 text-center'>
                 <td className='px-16 py-2 flex flex-row items-center'>
                     <img className="h-8 w-8 rounded-full object-cover" src={avatar || '#'} alt=''/>
-                    <span className='text-center ml-2 font-semibold'>{name || unknown}</span>
+                    <span className='text-center ml-2 font-semibold'>{name || "Unknown"}</span>
                 </td>
                 <td className='px-16 py-2'>
-                    <span>{email|| unknown}</span>
+                    <span>{email|| "Unknown"}</span>
                 </td>
                 <td className='px-16 py-2'>
-                    <span>{salary|| unknown}</span>
+                    <span>{salary|| "Unknown"}</span>
                 </td>
                 <td className='px-16 py-2'>
-                    <span>{date|| unknown}</span>
+                    <span>{date|| "Unknown"}</span>
                 </td>
                 <td className='px-16 py-2'>
-                    <button className='cursor'><span className='bg-green-500 text-white px-5 py-1 rounded-full'>{status||unknown}</span></button>
+                <button className="cursor"><span className={`${status == "Active" ? 'bg-green-500' : 'bg-rose-500'} text-white px-5 py-1 rounded-full`}>{status || "Unknown"}</span></button>
                 </td>
                 <td className='px-16 py-2 flex justify-around gap-5'>
                     <button className='cursor'><FiEdit size={25} color="rgb(32,197,94)"/></button>
