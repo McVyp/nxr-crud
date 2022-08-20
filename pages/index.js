d276b98efdad6eb0b90d612ffbed2055818fad24
lyelc <lyeclyde@gmail.com>
2022-08-20 19:39:58 +0545

    renamed files

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { BiCheck, BiUserPlus, BiX } from "react-icons/bi";
-import Table from '../components/Table';
-import Form from '../components/Form';
+import Table from '../components/table';
+import Form from '../components/form';
 import { useState } from 'react';
 import { useSelector, useDispatch  } from 'react-redux';
 import { toggleChangeAction, deleteAction } from '../redux/reducer';
